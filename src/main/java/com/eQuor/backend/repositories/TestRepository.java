58f22db76eef51b5edec1d9c3c9854406bf6fd0d
Senith Uthsara <senithkarunarathneu@gmail.com>
2023-09-11 20:59:39 +0530

    change

--- src/main/java/com/eQuor/backend/repositories/TestRepository.java
+++ src/main/java/com/eQuor/backend/repositories/TestRepository.java
@@ -2,6 +2,8 @@ package com.eQuor.backend.repositories;
 
 import com.eQuor.backend.models.Test;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface TestRepository extends JpaRepository<Test,Integer> {
 }
