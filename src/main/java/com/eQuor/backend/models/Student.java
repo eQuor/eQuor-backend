0d279a943b0bf62db39ce36d184ed8e120a1c158
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-09-28 01:09:19 +0530

    Update Student.java

--- src/main/java/com/eQuor/backend/models/Student.java
+++ src/main/java/com/eQuor/backend/models/Student.java
@@ -46,7 +46,8 @@ public class Student {
     @Column(name = "role")
     private Role role;
 
-
+    @Column(name = "device_qr")
+    String qrCode;
 
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(role.name()));
