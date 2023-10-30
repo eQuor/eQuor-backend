22995ec332fa67c7a78ea2cd5e7cadf3dc57b65d
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-10-30 10:23:17 +0530

    Revert "csv file upload"
    This reverts commit 6b71b24abf371c58198f66b9169221c050cb6e6f.

--- src/main/java/com/eQuor/backend/configurations/RepositoryConfig.java
+++ src/main/java/com/eQuor/backend/configurations/RepositoryConfig.java
@@ -2,6 +2,8 @@ package com.eQuor.backend.configurations;
 
 import com.eQuor.backend.models.*;
 import com.eQuor.backend.models.Module;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.stereotype.Component;
@@ -12,6 +14,6 @@ public class RepositoryConfig implements RepositoryRestConfigurer {
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
-        config.exposeIdsFor(Staff.class, APIRequest.class, Test.class, Lecturer.class, Student.class, Module.class, LecturerRegisterModule.class,StudentRegisterModule.class);
+        config.exposeIdsFor(Staff.class, APIRequest.class, Test.class, Lecturer.class, Student.class, Module.class);
     }
 }
