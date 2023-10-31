2e1fd199370b338ff9509eab6154a68eef633a35
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-10-31 15:08:31 +0530

    Update LecturerRegisterModule.java

--- src/main/java/com/eQuor/backend/models/LecturerRegisterModule.java
+++ src/main/java/com/eQuor/backend/models/LecturerRegisterModule.java
@@ -1,10 +1,7 @@
 package com.eQuor.backend.models;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -18,8 +15,10 @@ import lombok.*;
 
 public class LecturerRegisterModule {
 
-
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @Column(name = "lecturer_id")
     @JsonInclude(JsonInclude.Include.ALWAYS)
     String lecturer_id;
@@ -27,7 +26,5 @@ public class LecturerRegisterModule {
     @Column(name = "module_id")
     Integer module_id;
 
-    public String getId() {
-        return lecturer_id;
-    }
+    
 }
