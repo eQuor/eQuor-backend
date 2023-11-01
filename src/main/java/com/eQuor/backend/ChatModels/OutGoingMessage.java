6b7f13a4926d819333330ac0b988cd4bc25a107b
Senith Uthsara <senithkarunarathneu@gmail.com>
2023-11-01 13:06:45 +0530

    changes

---
+++ src/main/java/com/eQuor/backend/ChatModels/OutGoingMessage.java
@@ -0,0 +1,17 @@
+package com.eQuor.backend.ChatModels;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OutGoingMessage {
+    private String content;
+
+
+
+}
